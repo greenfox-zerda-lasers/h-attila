baf0b1702b80d532aeb4714559a843400d100a30
h-attila <email.atinak@gmail.com>
2016-12-14 20:16:44 +0100

    w8d3 - working on 03 but server down

---
+++ week-08/day-3/03_movie_post/03.js
@@ -0,0 +1,11 @@
+var sendButton = document.querySelector('.send-button');
+sendButton.addEventListener('click', function(){
+  postToWeb();
+});
+
+function postToWeb(){
+//  var httpRequest = new XMLHttpRequest();
+//  httpRequest.open("POST", 'https://sheetsu.com/apis/v1.0/7d4845a11814', true);
+//  httpRequest.send();
+  alert('posting in progress');
+}
