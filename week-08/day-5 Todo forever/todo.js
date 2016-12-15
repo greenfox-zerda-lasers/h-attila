3c6d5ced1c18aa433e987d9336990a45ea971770
h-attila <email.atinak@gmail.com>
2016-12-15 18:16:51 +0100

    w804 - ToDo functions working

--- week-08/day-5 Todo forever/todo.js
+++ week-08/day-5 Todo forever/todo.js
@@ -17,7 +17,7 @@ APP.prototype.open = function (){
   httpRequest.send();
   httpRequest.onreadystatechange = function(){
     if (httpRequest.readyState === XMLHttpRequest.DONE){
-      var response = JSON.parse(httpRequest.response);
+      var response = JSON.parse(httpRequest.response).reverse();
       app.list(response);
     }
   };
@@ -27,22 +27,47 @@ APP.prototype.list = function(response){
   var todoListElement = document.querySelector('ul');
   this.todoText = "";
   response.forEach(function(listItem){
-    this.todoText += '<li><div class="todo-text">' + listItem.text + '</div><div class="todo-dashbin"> </div><div class="todo-check"> </div></li>';
+    this.todoText += '<li><div class="todo-text">' + listItem.text + '</div><div id="' + listItem.id + '" class="todo-dashbin"> </div><div id="' + listItem.id + '" class="todo-check"> </div></li>';
   }, this);
   todoListElement.innerHTML = this.todoText;
-  
   var checkBoxes = document.querySelectorAll('.todo-check');
   checkBoxes.forEach(function(item, index){
     if (response[index].completed === true){
-      item.style.backgroundImage = 'url("success_ok.svg")';
+      item.style.backgroundImage = 'url("images/success_ok.svg")';
     } else {
-      item.style.backgroundImage = 'url("oval.svg")';
+      item.style.backgroundImage = 'url("images/oval.svg")';
     }
+    item.addEventListener('click', function(){
+      app.update(item.id, response[index].completed, response[index].text);
+    });
   });
-};
 
-APP.prototype.update = function(){
-  console.log('update');
+  var checkBin = document.querySelectorAll('.todo-dashbin');
+  checkBin.forEach(function(item){
+    item.addEventListener('click', function(){
+      app.delete(item.id);
+    });
+});
+
+APP.prototype.update = function(id, state, text){
+  var dataToUpload = {};
+  dataToUpload.text = text;
+  if (state === true){
+    dataToUpload.completed = false;
+  } else {
+    dataToUpload.completed = true;
+  }
+  console.log(dataToUpload);
+  var httpPost = new XMLHttpRequest();
+  httpPost.open('PUT', 'https://mysterious-dusk-8248.herokuapp.com/todos/' + id);
+  httpPost.setRequestHeader('Content-Type', 'application/json');
+  httpPost.send(JSON.stringify(dataToUpload));
+  httpPost.onreadystatechange = function(){
+    if (httpPost.readyState === XMLHttpRequest.DONE){
+      app.open();
+    }
+  };
+};
 };
 
 APP.prototype.add = function(item){
@@ -60,15 +85,16 @@ APP.prototype.add = function(item){
   };
 };
 
-APP.prototype.delete = function(item){
-  console.log('delete');
-  dataToUpload = 'https://mysterious-dusk-8248.herokuapp.com/todos/' + item;
-
+APP.prototype.delete = function(id){
+  dataToUpload = 'https://mysterious-dusk-8248.herokuapp.com/todos/' + id;
   var httpPost = new XMLHttpRequest();
   httpPost.open('DELETE', dataToUpload);
   httpPost.setRequestHeader('Content-Type', 'application/json');
   httpPost.send();
-  app.open();
+  httpPost.onreadystatechange = function(){
+    if (httpPost.readyState === XMLHttpRequest.DONE){
+      app.open();
+}};
 };
 
 // ***** START main function *****
