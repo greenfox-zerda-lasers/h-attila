e3ca8d8b13c9168608660a4e463fee8af58ed993
h-attila <email.atinak@gmail.com>
2016-12-18 22:37:55 +0100

    w7d4 - all refractored and ready

--- week-07/day-4/workshop/content-2.js
+++
@@ -1,4 +0,0 @@
-var newText = document.querySelectorAll('p');
-for (var i=0; i < newText.length; i++){
-  newText[i].textContent = newText[newText.length-1].textContent;
-}
--- week-07/day-4/workshop/content-3.js
+++
@@ -1,3 +0,0 @@
-var newTextContent = document.querySelectorAll('p');
-document.querySelector(".output1").textContent = newTextContent[0].textContent;
-document.querySelector(".output2").innerHTML = newTextContent[0].innerHTML;
