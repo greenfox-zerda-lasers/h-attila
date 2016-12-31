eb949ab63101a0d599f3427e022bacdcc89d476b
h-attila <email.atinak@gmail.com>
2016-12-31 01:27:51 +0100

    w8d5 - ToDo refractored and ready

--- week-08/day-5_Todo_forever/todo.js
+++ week-08/day-5_Todo_forever/todo.js
@@ -1,41 +1,62 @@
-// ***** button click for future *****
-var addButton = document.querySelector('.button');
-
-addButton.addEventListener('click', function(){
-  var inputFieldText = document.querySelector('.inputField');
-  var inputText = inputFieldText.value;
-  inputFieldText.value = '';
-  app.add(inputText);
-});
-
-// ***** APP object create *****
-function APP(){
-  this.todoText = '';
-}
-
-APP.prototype.open = function (){
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos');
-  httpRequest.send();
-  httpRequest.onreadystatechange = function(){
-    if (httpRequest.readyState === XMLHttpRequest.DONE){
-      var response = JSON.parse(httpRequest.response).reverse();
-      app.list(response);
-    }
+// GET DATA FROM WEB
+  var list = function(){
+    talkToServer('GET', '/todos', '', updateTodoList);
   };
-};
 
-APP.prototype.list = function(response){
-  var todoListElement = document.querySelector('ul');
-  this.todoText = "";
-  response.forEach(function(listItem){
-    this.todoText += '<li><div class="todo-text">' + listItem.text + '</div><div id="' + listItem.id + '" class="todo-dashbin"> </div><div id="' + listItem.id + '" class="todo-check"> </div></li>';
-  }, this);
+// UPDATE DATA STATUS ON WEB
+  var update = function(id, status, text){
+    let data = JSON.stringify({ "text" : text, "completed" : status });
+    talkToServer('PUT', '/todos/' + id, data, list);
+  };
 
-  todoListElement.innerHTML = this.todoText;
+// ADD NEW TODO DATA TO WEB
+  var add = function(newTodoText){
+    let data = JSON.stringify({ "text" : newTodoText });
+    console.log(data);
+    talkToServer('POST', '/todos', data, list);
+  };
 
+// DELETE TODO DATA FROM WEB
+  var deleteTodo = function(id){
+    let data = JSON.stringify({});
+    talkToServer('DELETE', '/todos/' + id, data, list);
+  };
+
+// SETTING UP SERVER COMMUNICATION
+  var talkToServer = function(method, additionalUrl, data, callbackFunc){
+    let url = 'https://mysterious-dusk-8248.herokuapp.com' + additionalUrl;
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open(method, url, true);
+    httpRequest.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    httpRequest.send(data);
+    httpRequest.onreadystatechange = function(){
+      if (httpRequest.readyState === XMLHttpRequest.DONE){
+        callbackFunc(JSON.parse(httpRequest.response));
+      }
+    };
+  };
+
+// ADD BUTTON CREATE
+  var addButton = document.querySelector('.button');
+  addButton.addEventListener('click', function(){
+    var inputFieldText = document.querySelector('.inputField');
+    var newTodoText = inputFieldText.value;
+    inputFieldText.value = '';
+    console.log(newTodoText);
+    add(newTodoText);
+  });
+
+// CREATE AND DISPLAY TODO LIST
+  var updateTodoList = function(response){
+    this.todoText = "";
+    var todoListElement = document.querySelector('ul');
+    response.forEach(function(listItem){
+      this.todoText += '<li><div class="todo-text">' + listItem.text + '</div><div id="' + listItem.id + '" class="todo-dashbin"> </div><div id="' + listItem.id + '" class="todo-check"> </div></li>';
+    }, this);
+    todoListElement.innerHTML = this.todoText;
+
+// CHECKBOX EVENT HANDLING
   var checkBoxes = document.querySelectorAll('.todo-check');
-
   checkBoxes.forEach(function(item, index){
     if (response[index].completed === false){
       item.className = 'todo-check';
@@ -43,82 +64,34 @@ APP.prototype.list = function(response){
       item.className = 'todo-checked';
     }
     item.addEventListener('click', function(){
-      app.update(item.id, response[index].completed, response[index].text);
+      update(item.id, !response[index].completed, response[index].text);
     });
   });
 
+// DELETE BUTTON EVENT HANDLING
   var checkBin = document.querySelectorAll('.todo-dashbin');
   checkBin.forEach(function(item){
     item.addEventListener('click', function(){
-      app.delete(item.id);
+      deleteTodo(item.id);
+      });
     });
-});
-
-APP.prototype.update = function(id, state, text){
-  var dataToUpload = {};
-  dataToUpload.text = text;
-  if (state === true){
-    dataToUpload.completed = false;
-  } else {
-    dataToUpload.completed = true;
-  }
-  console.log(dataToUpload);
-  var httpPost = new XMLHttpRequest();
-  httpPost.open('PUT', 'https://mysterious-dusk-8248.herokuapp.com/todos/' + id);
-  httpPost.setRequestHeader('Content-Type', 'application/json');
-  httpPost.send(JSON.stringify(dataToUpload));
-  httpPost.onreadystatechange = function(){
-    if (httpPost.readyState === XMLHttpRequest.DONE){
-      app.open();
-    }
   };
-};
-};
 
-APP.prototype.add = function(item){
-  dataToUpload = {};
-  dataToUpload.text = item;
-  console.log(item);
+// STYLE SETTINGS
+  var settingsMenu = document.querySelector('.settings');
+  var stylesheet = document.querySelector('#stylesheet');
+  var stylechecker = 0;
 
-  var httpPost = new XMLHttpRequest();
-  httpPost.open('POST', 'https://mysterious-dusk-8248.herokuapp.com/todos');
-  httpPost.setRequestHeader('Content-Type', 'application/json');
-  httpPost.send(JSON.stringify(dataToUpload));
-  httpPost.onreadystatechange = function(){
-    if (httpPost.readyState === XMLHttpRequest.DONE){
-      app.open();
+  settingsMenu.addEventListener('click', function(){
+    var styleList = [ "css/todo.css", "css/todo_1.css", "css/todo_2.css" ];
+    console.log(styleList[stylechecker], stylechecker);
+    stylechecker++;
+    if (stylechecker >= 3){
+      stylechecker = 0;
     }
-  };
-};
+    stylesheet.href=styleList[stylechecker];
+  });
 
-APP.prototype.delete = function(id){
-  dataToUpload = 'https://mysterious-dusk-8248.herokuapp.com/todos/' + id;
-  var httpPost = new XMLHttpRequest();
-  httpPost.open('DELETE', dataToUpload);
-  httpPost.setRequestHeader('Content-Type', 'application/json');
-  httpPost.send();
-  httpPost.onreadystatechange = function(){
-    if (httpPost.readyState === XMLHttpRequest.DONE){
-      app.open();
-}};
-};
+// START MAIN FUNCTION
 
-// ***** START main function *****
-
-var app = new APP();
-app.open();
-
-var settingsMenu = document.querySelector('.settings');
-var stylesheet = document.querySelector('#stylesheet');
-var stylechecker = 0;
-
-
-settingsMenu.addEventListener('click', function(){
-  var styleList = [ "css/todo.css", "css/todo_1.css", "css/todo_2.css" ];
-  console.log(styleList[stylechecker], stylechecker);
-  stylechecker++;
-  if (stylechecker >= 3){
-    stylechecker = 0;
-  }
-  stylesheet.href=styleList[stylechecker];
-});
+  list();
