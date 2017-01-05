bbbc2782bc1087995c2f45f1119e3f470121d055
h-attila <email.atinak@gmail.com>
2017-01-05 20:33:28 +0100

    w10 - front, server almost work well

--- week-10/foxplayer_server.js
+++ week-10/foxplayer_server.js
@@ -11,102 +11,106 @@ app.use('/', server.static('web'));
 var tracks = [
 {
   id : 1,
-  title : 'Never Give Up',
-  path : 'music/Ars_Sonor_-_02_-_Never_Give_Up.mp3',
-  author : 'Ars Sonor',
-  length : '1:20',
-  playlist_id: 1,
+  title : "Never Give Up",
+  path : "music/Ars_Sonor_-_02_-_Never_Give_Up.mp3",
+  author : "Ars Sonor",
+  length : "1:20",
+  playlist_id: [1],
 },
 {
   id : 2,
-  title : 'Doctor Talos Answers The Door',
-  path : 'music/Doctor_Turtle_-_Doctor_Talos_Answers_The_Door.mp3',
-  author : 'Doctor Turtle',
-  length : '2:10',
-  playlist_id: 1,
+  title : "Doctor Talos Answers The Door",
+  path : "music/Doctor_Turtle_-_Doctor_Talos_Answers_The_Door.mp3",
+  author : "Doctor Turtle",
+  length : "2:10",
+  playlist_id: [1],
 },
 {
   id : 3,
-  title : 'Purple Drift',
-  path : 'music/Organoid_-_09_-_Purple_Drift.mp3',
-  author : 'Organoid',
-  length : '3:30',
-  playlist_id: 0,
+  title : "Purple Drift",
+  path : "music/Organoid_-_09_-_Purple_Drift.mp3",
+  author : "Organoid",
+  length : "3:30",
+  playlist_id: [1],
 }];
 
 // PLAYLISTS DATA
 var playlists = [
 {
-  id : 1,
-  playlistName : 'My first playlist',
-  system : 'wtf',
+  id: 1,
+  playlistName: "All tracks",
+  system: "wtf",
 },
 {
-  id : 2,
-  playlistName : 'My second playlist',
-  system : 'wtf',
+  id: 2,
+  playlistName: "Favorites",
 },
 {
-  id : 3,
-  playlistName : 'My 3 playlist',
-  system : 'wtf',
+  id: 3,
+  playlistName: "1 playlist",
 },
 {
-  id : 4,
-  playlistName : 'My 4 playlist',
-  system : 'wtf',
+  id: 4,
+  playlistName: "2 playlist",
 },
 ];
 
-// GET PLAYLISTS FROM SERVER
+// GET PLAYLISTS FROM SERVER ** api ok
 app.get('/playlists', function (req, res) {
-  res.send(playlists);
+  res.json(playlists);
 });
 
-// POST NEW PLAYLIST TO THE SERVER
+// POST NEW PLAYLIST TO THE SERVER - ADD NEW PLAYLIST
 app.post('/playlists', function (req, res) {
-  playlists.push(req.body);
-  res.send(playlists);
+  console.log(req.body);
+  var newId = playlists[playlists.length-1].id + 1;
+  var newPlaylistItem = { "id": newId, "playlistName": req.body.value};
+  playlists.push(newPlaylistItem);
+  res.json(playlists);
 });
 
-// DELETE PLAYLIST BY ID
+// DELETE PLAYLIST BY ID ** api ok
 app.delete('/playlists/:id', function (req, res) {
   playlists.forEach (function (playlistItem, index) {
     if (playlistItem.id === parseInt(req.params.id)) {
       playlists.splice(index, 1);
     }
   });
-  res.send(playlists);
+  res.json(playlists);
 });
 
-// GET ALL TRACKS
+// GET ALL TRACKS ** api ok
 app.get('/playlis-tracks/', function (req, res) {
   res.json(tracks);
 });
 
-// LIST ALL THE TRACKS ADDED TO THE PLAYLIST
+// LIST THE TRACKS OF THE PLAYLIST
 app.get('/playlis-tracks/:playlist_id', function (req, res) {
-  var responseData = tracks.filter( function (item) {
+  var playlists = tracks.filter( function (item) {
     return item.playlist_id === parseInt(req.params.playlist_id);
   });
-  res.send(responseData);
+  res.json(playlists);
 });
 
-// ADDS THE TRACK TO THE ID'S PLAYLIST
-app.post('/playlist-tracks/:playlist_id', function (req, res) {
+// ADD NEW TRACK TO THE PLAYLIST BY ID
+app.post('/playlist-tracks/:playlist_id/:track_id', function (req, res) {
   tracks.forEach (function (trackItem, index) {
-    if (trackItem.id === parseInt(req.params.playlist_id)) {
-      trackItem.playlist_id = req.params.playlist_id;
+    if (trackItem.id === parseInt(req.params.track_id)) {
+      if (trackItem.playlist_id.each(function(){
+      //   return
+      }));
+      trackItem.playlist_id = parseInt(req.params.playlist_id);
+      console.log(trackItem, req.params.playlist_id);
     }
   });
-  res.send(tracks);
+  res.json(tracks);
 });
 
 // DELETE THE TRACK FROM THE PLAYLIST
 app.delete('/playlist-tracks/:playlist_id/:track_id', function (req, res) {
   tracks.forEach (function (trackItem, index) {
     if (trackItem.id === parseInt(req.params.track_id)) {
-      trackItem.playlist_id = 0;
+      trackItem.playlist_id = 1;
     }
   });
   res.send(tracks);
--- week-10/web/scripts/foxplayer.js
+++ week-10/web/scripts/foxplayer.js
@@ -1,157 +1,262 @@
-// AUDIO DATA
-var actualTrackID = 0;
 
-var audioPlayer = document.querySelector('audio');
-console.log(audioPlayer.src);
+// **********************
+// MAIN APP DEFINITIONS
+// **********************
+var app = {
 
-var playList = [{
-  id : 0,
-  name : 'All tracks',
-  tracks : [],
-},
-{
-  id : 1,
-  name : 'Favorites',
-  tracks : [],
-},
-{
-  id : 3,
-  name : 'Relaxing music for programming',
-  tracks : [],
-},
-{
-  id : 4,
-  name : 'Party',
-  tracks : [],
-}];
+  // CURRENT PLAYED TRACK
+  actualTrackID : 0,
+  actualPlayListID: 1,
 
-var playTracks = [
-{
-  id : 1,
-  title : 'Never Give Up',
-  file : 'music/Ars_Sonor_-_02_-_Never_Give_Up.mp3',
-  author : 'Ars Sonor',
-  length : '1:20',
-},
-{
-  id : 2,
-  title : 'Doctor Talos Answers The Door',
-  file : 'music/Doctor_Turtle_-_Doctor_Talos_Answers_The_Door.mp3',
-  author : 'Doctor Turtle',
-  length : '2:10',
-},
-{
-  id : 3,
-  title : 'Purple Drift',
-  file : 'music/Organoid_-_09_-_Purple_Drift.mp3',
-  author : 'Organoid',
-  length : '3:30',
-}];
+  // CURRENT TRACKLIST
+  playTracks : [],
 
-// LIST PLAYLISTS ON LEFT PANEL
-var playLists = document.querySelector('.left-playlist');
-playList.forEach (function(playlist, index) {
-  var playlistItem = document.createElement('div');
-  var playlistName = document.createElement('p');
-  var playlistItemDelete = document.createElement('p');
+  // AUDIO DATA
+  audioPlayer : document.querySelector('audio'),
 
-  playlistItem.className = 'playlist-item';
-  playlistName.className = 'playlist-name';
-  playlistName.id = index;
-  playlistName.innerHTML = playlist.name;
-  playlistItem.appendChild(playlistName);
 
-  // 'All TRACKS' AND 'FAVORITES' CAN NOT DELETE
-  if (index >= 2) {
-    playlistItemDelete.className = 'playlist-delete';
-    playlistItemDelete.id = index;
-    playlistItem.appendChild(playlistItemDelete);
+  // app STARTS, GET INIT DATA
+  init: function(){
+    ajax.talkToServer('GET', '/playlists', null, app.playlistsCreate);
+    ajax.talkToServer('GET', '/playlis-tracks/', null, app.trackListCreate);
+  },
 
-    // LISTEN FOR PLAYLIST DELETE
-    playlistItemDelete.addEventListener('click', function(item){
-      talkToServer('DELETE', '/playlists/' + id, item, console.log);
+  // LOGO, NOTIFICATIONS
+  logo: function(){
+    // logo, notifications comes here
+  },
+
+  // CREATE, LIST PLAYLISTS ON LEFT PANEL
+  playlistsCreate: function(playList){
+    var playLists = document.querySelector('.left-playlist');
+    playLists.innerHTML = '';
+
+    // CREATE PLAYLISTS
+    playList.forEach (function(item, index) {
+      var playlistItem = document.createElement('div');
+      var playlistName = document.createElement('p');
+      var playlistItemDelete = document.createElement('p');
+
+      playlistItem.className = 'playlist-item';
+      playlistName.className = 'playlist-name';
+      playlistName.id = item.id;
+      playlistName.innerHTML = item.playlistName;
+      playlistItem.appendChild(playlistName);
+
+      // 'All TRACKS' AND 'FAVORITES' CAN NOT DELETE
+      if (index >= 2) {
+        playlistItemDelete.className = 'playlist-delete';
+        playlistItemDelete.id = item.id;
+        playlistItem.appendChild(playlistItemDelete);
+
+      // DELETE PLAYLIST BY ID - EVENT LISTENER
+        playlistItemDelete.addEventListener('click', function(){
+            ajax.talkToServer('DELETE', '/playlists/' + this.id, null, app.playlistsCreate);
+        });
+      }
+
+        // LIST THE TRACKS OF THE PLAYLIST - EVENT LISTENER
+        playlistName.addEventListener('click', function(){
+          app.actualPlayListID = this.id;
+          console.log(app.actualPlayListID);
+          ajax.talkToServer('GET', '/playlis-tracks/' + this.id, null, app.trackListCreate);
+        });
+        playLists.appendChild(playlistItem);
     });
+  },
+
+    // LIST TRACKS ON RIGHT PANEL
+    trackListCreate : function (playTracks){
+      app.playTracks = playTracks;
+
+      var trackItems = document.querySelector('.right-songs');
+      trackItems.innerHTML = '';
+      playTracks.forEach (function(track, index) {
+        var newTrack = document.createElement('div');
+        var trackId = document.createElement('p');
+        var trackTitle = document.createElement('p');
+        var trackLength = document.createElement('p');
+
+        newTrack.className = 'track-item';
+        newTrack.id = track.id;
+        trackId.className = 'track-id';
+        trackTitle.className = 'track-title';
+        trackLength.className = 'track-length';
+
+        trackId.innerHTML = track.id;
+        trackTitle.innerHTML = track.title;
+        trackLength.innerHTML = track.length;
+
+        newTrack.appendChild(trackId);
+        newTrack.appendChild(trackTitle);
+        newTrack.appendChild(trackLength);
+        newTrack.addEventListener('click', function (item) {
+          app.actualTrackID = track.id;
+          app.audioPlayer.setAttribute('src', app.playTracks[index].path);
+          actualSongTitle.innerHTML = app.playTracks[index].title;
+          actualSongAuthor.innerHTML = app.playTracks[index].author;
+        });
+        trackItems.appendChild(newTrack);
+      });
+    },
+};
+
+// ***********************
+//  SERVER COMMUNICATION
+// ***********************
+
+var ajax = {
+
+  // SETTING UP SERVER COMMUNICATION
+  talkToServer : function(method, additionalUrl, data, callbackFunc){
+    var url = 'http://localhost:3000' + additionalUrl;
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open(method, url, true);
+    httpRequest.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+    // IF data EXIST
+    data ? httpRequest.send(JSON.stringify(data)) : httpRequest.send();
+
+    httpRequest.onreadystatechange = function(){
+      if (httpRequest.readyState === XMLHttpRequest.DONE){
+        callbackFunc(JSON.parse(httpRequest.response));
+      }
+    };
   }
+};
 
-  // LISTEN FOR PLAYLIST
-  playlistName.addEventListener('click', function(){
-    console.log(this.id);
-  });
+// *****************
+//  EVENT HANDLING
+// *****************
 
-  playLists.appendChild(playlistItem);
-});
-
-// LIST TRACKS ON RIGHT PANEL
-var tracks = document.querySelector('.right-songs');
-playTracks.forEach (function(track, index) {
-  var newTrack = document.createElement('div');
-  var trackId = document.createElement('p');
-  var trackTitle = document.createElement('p');
-  var trackLength = document.createElement('p');
-
-  newTrack.className = 'track-item';
-  newTrack.id = index;
-  trackId.className = 'track-id';
-  trackTitle.className = 'track-title';
-  trackLength.className = 'track-length';
-
-  trackId.innerHTML = index + 1;
-  trackTitle.innerHTML = track.title;
-  trackLength.innerHTML = track.length;
-
-  newTrack.appendChild(trackId);
-  newTrack.appendChild(trackTitle);
-  newTrack.appendChild(trackLength);
-  newTrack.addEventListener('click', function (item) {
-    console.log(this.id);
-  });
-  tracks.appendChild(newTrack);
-});
-
-// EVENT HANDLING
+// POPUP WINDOW HANDLING
+// ADD NEW PLAYLIST
 var addNewPlaylist = document.querySelector('.add-new-playlist');
 addNewPlaylist.addEventListener('click', function(){
-  console.log('add new playlist');
+
+  // CREATE NEW PLAYLIST - DIALOG BOX
+  vex.dialog.prompt({
+    message: 'Please add the new list name',
+    placeholder: 'newlist',
+    callback: function (value) {
+      ajax.talkToServer('POST', '/playlists', { value }, app.playlistsCreate);
+      }
+  });
 });
 
+// ADD NEW TRACK TO THE PLAYLIST ** not work!
 var addToPlaylist = document.querySelector('.add-to-playlist');
 addToPlaylist.addEventListener('click', function(){
-  console.log('add sont to playlist');
+  console.log(app.playTracks[app.actualTrackID].id);
+  ajax.talkToServer('POST', '/playlist-tracks/' + app.actualPlayListID + '/', app.playTracks[app.actualTrackID].id, null, app.trackListCreate);;
 });
 
+// ADD NEW TRACK TO THE FAVORITES
 var addToFavorites = document.querySelector('.add-to-favorites');
-addToFavorites.addEventListener('click', function(){
-  console.log('add song to favorites');
+addToFavorites.addEventListener('click', function (track) {
+  ajax.talkToServer('POST', '/playlist-tracks/2/' + app.actualTrackID, null, app.trackListCreate);
 });
+
+// **************************
+// AUDIO PLAYER TIME EVENTS
+// **************************
+
+var audioCurrentTime = document.querySelector('.current-time');
+var totalLength = document.querySelector('.total-length');
+
+// SEEK BAR FOR MEDIA PLAY
+// Initialize a new plugin instance for element or array of elements.
+var slider = document.querySelector('.range-seek');
+rangeSlider.create(slider, {
+    polyfill: true,     // Boolean, if true, custom markup will be created
+    rangeClass: 'rangeSlider',
+    disabledClass: 'rangeSlider--disabled',
+    fillClass: 'rangeSlider__fill',
+    bufferClass: 'rangeSlider__buffer',
+    handleClass: 'rangeSlider__handle',
+    startEvent: ['mousedown', 'touchstart', 'pointerdown'],
+    moveEvent: ['mousemove', 'touchmove', 'pointermove'],
+    endEvent: ['mouseup', 'touchend', 'pointerup'],
+    min: null,          // Number , 0
+    max: null,          // Number, 100
+    step: null,         // Number, 1
+    value: null,        // Number, center of slider
+    buffer: null,       // Number, in percent, 0 by default
+    stick: null,        // [Number stickTo, Number stickRadius] : use it if handle should stick to stickTo-th value in stickRadius
+    borderRadius: 10,    // Number, if you use buffer + border-radius in css for looks good,
+
+    onSlide: function (position, value) {
+      seek(value);
+    },
+});
+
+app.audioPlayer.addEventListener('timeupdate',function(){
+  var timeSecFromMillisec = function (seconds) {
+    sec = Math.floor( seconds );
+    min = Math.floor( sec / 60 );
+    min = min >= 10 ? min : '0' + min;
+    sec = Math.floor( sec % 60 );
+    sec = sec >= 10 ? sec : '0' + sec;
+    return min + ':' + sec;
+  };
+
+  if (!isNaN(app.audioPlayer.duration)) {
+    audioCurrentTime.innerHTML = "<p>" + timeSecFromMillisec(app.audioPlayer.currentTime) + "</p>";
+    totalLength.innerHTML = "<p>" + timeSecFromMillisec(app.audioPlayer.duration) + "</p>";
+
+    if (app.audioPlayer.duration === app.audioPlayer.currentTime && app.actualTrackID < app.playTracks.length-1) {
+      console.log("next track!");
+      loadNextTrack();
+    }
+    slider.rangeSlider.update({ value: app.audioPlayer.currentTime/app.audioPlayer.duration*100});
+  }
+
+}, false);
+
+
+// *********************
+// AUDIO PLAYER EVENTS
+// *********************
+
+
+// expects between 0-100 value
+function seek( percent ){
+  app.audioPlayer.currentTime = app.audioPlayer.duration * percent;
+}
 
 var prevSong = document.querySelector('.prev');
 prevSong.addEventListener('click', function(){
-  actualTrackID -= 1;
-  audioPlayer.setAttribute('src', playTracks[actualTrackID].file);
-  actualSongTitle.innerHTML = playTracks[actualTrackID].title;
-  actualSongAuthor.innerHTML = playTracks[actualTrackID].author;
-  console.log(playTracks[actualTrackID].file);
+  if (app.actualTrackID > 0) {
+      app.actualTrackID -= 1;
+  }
+  app.audioPlayer.setAttribute('src', app.playTracks[app.actualTrackID].path);
+  actualSongTitle.innerHTML = app.playTracks[app.actualTrackID].title;
+  actualSongAuthor.innerHTML = app.playTracks[app.actualTrackID].author;
 });
 
 var pauseSong = document.querySelector('.pause');
 pauseSong.addEventListener('click', function(){
-  console.log(audioPlayer.currentTime, audioPlayer.duration);
-  if (audioPlayer.paused) {
-    audioPlayer.play();
+  if (app.audioPlayer.paused) {
+    app.audioPlayer.play();
     pauseSong.style.backgroundImage = "url(images/pause.svg)";
   } else {
-    audioPlayer.pause();
+    app.audioPlayer.pause();
     pauseSong.style.backgroundImage = "url(images/play.svg)";
   }
 });
 
+var loadNextTrack = function () {
+  if (app.actualTrackID < app.playTracks.length-1) {
+      app.actualTrackID += 1;
+  }
+  app.audioPlayer.setAttribute('src', app.playTracks[app.actualTrackID].path);
+  actualSongTitle.innerHTML = app.playTracks[app.actualTrackID].title;
+  actualSongAuthor.innerHTML = app.playTracks[app.actualTrackID].author;
+};
+
 var nextSong = document.querySelector('.next');
 nextSong.addEventListener('click', function(){
-  actualTrackID += 1;
-  audioPlayer.setAttribute('src', playTracks[actualTrackID].file);
-  actualSongTitle.innerHTML = playTracks[actualTrackID].title;
-  actualSongAuthor.innerHTML = playTracks[actualTrackID].author;
-  console.log(playTracks[actualTrackID].file);
+  loadNextTrack();
 });
 
 var shuffleSong = document.querySelector('.shuffle');
@@ -167,42 +272,8 @@ muteSong.addEventListener('click', function(){
 var actualSongTitle = document.querySelector('h2');
 var actualSongAuthor = document.querySelector('h3');
 
-
-// SEEK BAR FOR MEDIA PLAY
-// Initialize a new plugin instance for element or array of elements.
-var sliderSeek = document.querySelectorAll('input[type="range-seek"]');
-rangeSlider.create(sliderSeek, {
-    polyfill: true,     // Boolean, if true, custom markup will be created
-    rangeClass: 'rangeSlider',
-    disabledClass: 'rangeSlider--disabled',
-    fillClass: 'rangeSlider__fill',
-    bufferClass: 'rangeSlider__buffer',
-    handleClass: 'rangeSlider__handle',
-    startEvent: ['mousedown', 'touchstart', 'pointerdown'],
-    moveEvent: ['mousemove', 'touchmove', 'pointermove'],
-    endEvent: ['mouseup', 'touchend', 'pointerup'],
-    min: 0,          // Number , 0
-    max: 100,          // Number, 100
-    step: 1,         // Number, 1
-    value: 0,        // Number, center of slider
-    buffer: 0,       // Number, in percent, 0 by default
-    stick: 0,        // [Number stickTo, Number stickRadius] : use it if handle should stick to stickTo-th value in stickRadius
-    borderRadius: 1,    // Number, if you use buffer + border-radius in css for looks good,
-    //onInit: function () {
-    //    console.info('onInit');
-    // },
-    //onSlideStart: function (position, value) {
-    //    console.info('onSlideStart', 'position: ' + position, 'value: ' + value);
-    // },
-    onSlide: function (position, value) {
-        console.log('onSlide', 'position: ' + position, 'value: ' + value);
-    },
-    //onSlideEnd: function (position, value) {
-    //    console.warn('onSlideEnd', 'position: ' + position, 'value: ' + value);
-    //}
-});
-
 var sliderVolume = document.querySelectorAll('input[type="range-volume"]');
+
 rangeSlider.create(sliderVolume, {
     polyfill: true,     // Boolean, if true, custom markup will be created
     rangeClass: 'rangeSlider',
@@ -216,51 +287,39 @@ rangeSlider.create(sliderVolume, {
     min: 0,          // Number , 0
     max: 100,          // Number, 100
     step: 1,         // Number, 1
-    value: 0,        // Number, center of slider
+    value: 100,        // Number, center of slider
     buffer: 0,       // Number, in percent, 0 by default
     stick: 0,        // [Number stickTo, Number stickRadius] : use it if handle should stick to stickTo-th value in stickRadius
     borderRadius: 1,    // Number, if you use buffer + border-radius in css for looks good,
-    //onInit: function () {
-    //    console.info('onInit');
-    // },
-    //onSlideStart: function (position, value) {
-    //    console.info('onSlideStart', 'position: ' + position, 'value: ' + value);
-    // },
+
     onSlide: function (position, value) {
-        console.log('onSlide', 'position: ' + position, 'value: ' + value);
-        audioPlayer.volume = value;
+        app.audioPlayer.volume = value;
         if (value === 0){
           muteSong.style.backgroundImage = "url(images/mute_new.svg)";
           muteSong.style.backgroundSize = '20px';
+          this.value = 100;
         } else {
           muteSong.style.backgroundImage = "url(images/volume.svg)";
           muteSong.style.backgroundSize = '10px';
         }
-    },
-    //onSlideEnd: function (position, value) {
-    //    console.warn('onSlideEnd', 'position: ' + position, 'value: ' + value);
-    //}
+    }
 });
 
-// USE SEEK BAR FOR PLAY SONG
-//var giveMeSomeEvents = true; // or false
-//slider.rangeSlider.update({min : 0, max : 20, step : 0.5, value : 1.5, buffer : 70}, giveMeSomeEvents);
-// or
-//slider.rangeSlider.onSlideStart = function (position, value) {
-//                           console.error('anotherCallback', 'position: ' + position, 'value: ' + value);
-//                       };
 
+// THUMBNAIL ANIMATION
+var thumbnailsButton = document.querySelector('.panel-button');
+var thumbnailsPanel = document.querySelector('#thumbnails');
+thumbnailsButton.addEventListener('click', function(){
+  thumbnailsPanel.classList.toggle('panel-close');
+  if (thumbnailsPanel.classList.contains('panel-close')){
+    thumbnailsButton.style.transform = 'rotate(180deg)';
+  } else {
+    thumbnailsButton.style.transform = 'rotate(0deg)';
+  }
+});
 
-// SETTING UP SERVER COMMUNICATION
-  var talkToServer = function(method, additionalUrl, data, callbackFunc){
-    let url = 'http://localhost:3000' + additionalUrl;
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.open(method, url, true);
-    httpRequest.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-    httpRequest.send(data);
-    httpRequest.onreadystatechange = function(){
-      if (httpRequest.readyState === XMLHttpRequest.DONE){
-        callbackFunc(JSON.parse(httpRequest.response));
-      }
-    };
-  };
+//****************************
+//  MAIN PROGRAM STARTS HERE
+//****************************
+
+app.init();
